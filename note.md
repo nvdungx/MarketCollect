d3714ef38132a3e124d5ce9c7ea44ad97d4e4712
Dungnv <nvdung694@gmail.com>
2021-03-03 22:37:23 +0700

    update sample amazon 1

--- note.md
+++ note.md
@@ -4,3 +4,6 @@ Chrome:	https://sites.google.com/a/chromium.org/chromedriver/downloads
 Edge:	https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
 Firefox:	https://github.com/mozilla/geckodriver/releases
 Safari:	https://webkit.org/blog/6900/webdriver-support-in-safari-10/
+
+python -m venv .venv
+pip install -r requirement.txt
--- sample.py
+++ sample.py
@@ -26,23 +26,40 @@ class ItemStatus:
   IN_STOCK = 2
   OUT_STOCK = 3
 
+ITEM_STATUS = {
+  ItemStatus.NONE:"NONE",
+  ItemStatus.IN_STOCK:"In Stock",
+  ItemStatus.OUT_STOCK:"Out of Stock"
+}
+
 @dataclass
 class Product:
   site:int
+  row_idx:int
   link:str
   name:str
   price:float
   currency:str
   status:ItemStatus
+  status_str:str
   multi_vendor:bool
-  def __init__(self, _link="", _name="", _price=0, _cur="", _status=ItemStatus.NONE, _mul=False, _site=0):
+  def __init__(self, _link="", _name="", _price=0, _cur="", _status=ItemStatus.NONE, _mul=False, _site=0, _row_idx=1):
+    # product link
     self.link = _link
+    # product name
     self.name = _name
+    # price value
     self.price = _price
+    # currency
     self.currency = _cur
+    # ItemStatus
     self.status = _status
+    self.status_str = ""
+    # if product is provided by multiple vendor
     self.multi_vendor = _mul
+    # 5[ama] 2[ebay]
     self.site = _site
+    self.row_idx = _row_idx
 
 def get_price_ebay(prod_list):
 
@@ -54,6 +71,7 @@ def click_element(driver, xpath):
     return True
   except:
     return False
+
 def send_data(driver, xpath, data):
   try:
     wait(driver, 5).until(EC.presence_of_element_located((By.XPATH,xpath))).send_keys(data)
@@ -61,7 +79,17 @@ def send_data(driver, xpath, data):
   except:
     return False
 
+def get_element(driver, xpath):
+  try:
+    ele = driver.find_element_by_xpath(xpath)
+    return True, ele
+  except:
+    return False, None
+
 def get_price_amazon(prod_list):
+  # GENERAL ISSUE WITH THESE WEB crawl APPROACH: HTML structure of page could be change(when amazon change there page rendering)
+  # this lead to tool need to be re update with new xpath value for each element(name, price, etc..)
+
   # TODO: can get data faster with request(grequest), but has to change the zipcode to correct location with the request?
   #       issue: ip block with multiple request
   # header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"}
@@ -79,25 +107,110 @@ def get_price_amazon(prod_list):
   # options.add_argument("--headless")
   driver = webdriver.Chrome("./browserdriver/ChromeDriver88.0.4324.96_32b.exe", chrome_options=options)
   driver.get("https://amazon.com")
-
   # find zipcode element
-  click_element(driver, '//*[@id="nav-global-location-popover-link"]')
+  if(not click_element(driver, '//*[@id="nav-global-location-popover-link"]')):
+    raise Exception("WEB_DRIVER", "Failed to find amazon html element {0}".format("zip-code-button"))
   # //*[@id="GLUXZipUpdateInput"]
-  send_data(driver, '//*[@id="GLUXZipUpdateInput"]', "97124")
+  if(not send_data(driver, '//*[@id="GLUXZipUpdateInput"]', "97124")):
+    raise Exception("WEB_DRIVER", "Failed to find amazon html element {0}".format("zip-code-textbox"))
   # GLUXZipUpdate - update code button
-  click_element(driver, '//*[@id="GLUXZipUpdate"]')
+  if(not click_element(driver, '//*[@id="GLUXZipUpdate"]')):
+    raise Exception("WEB_DRIVER", "Failed to find amazon html element {0}".format("zip-code-update-button"))
   # continue - GLUXConfirmAction - /html/body/div[5]/div/div/div[2]/span/span
   if (False == click_element(driver,'//*/div[@class="a-popover-wrapper"]/div[@class="a-popover-footer"]/span/span')):
     # a-autoid-29-announce - apply code
-    click_element(driver,'//*[@id="a-autoid-29-announce"]')
-  for item in prod_list:
-    driver.get(item.link)
+    if(not click_element(driver,'//*[@id="a-autoid-29-announce"]')):
+      raise Exception("WEB_DRIVER", "Failed to find amazon html element {0}".format("zip-code-continue-button"))
 
+  for item_idx, item in enumerate(prod_list):
+    driver.get(item.link)
+    time.sleep(1)
+    # TITLE
+    # //*/div[@id="centerCol"]/*/[@id="productTitle"]
+    result, ele = get_element(driver, '//*/span[@id="productTitle"]')
+    if(not result):
+      raise Exception("WEB_DRIVER", "Failed to find amazon html element {0}".format("product-title"))
+    else:
+      item.name = ele.text.strip()
+    # PRICE
+    #             id="a-page"/id="dp"/id="dp-container"/id="ppd"/id="centerCol"/id="desktop_unifiedPrice"(div[10])/id="unifiedPrice_feature_div"/id="price"/class="a-lineitem"(table)
+    #                                                 id="priceblock_ourprice_row"(tr)/class="a-span12"(td[2])/id="priceblock_ourprice"
+    # /html/body/div[2]/div[3]/div[7]/div[5]/div[4]/div[10]/div/div/table/tbody/tr/td[2]/span[1]
+    # //*[@id="priceblock_ourprice"]
+    result, ele = get_element(driver, '//*/span[@id="priceblock_ourprice"]')
+    if(not result):
+      result, ele = get_element(driver, '//*/span[@id="priceblock_saleprice"]')
+      if (not result):
+        pass
+        # print("Failed to find amazon html element {0}".format("product-price"))
+    if(result):
+      item.multi_vendor = False
+      item.status = ItemStatus.IN_STOCK
+      temp = ele.text.strip()
+      reg = re.match(r"([^\d\.]*)([\d\.]+)([^\d\.]*)",temp)
+      if (reg != None):
+        item.price = float(str(reg[2]))
+        if(str(reg[1]).strip() != ""):
+          item.currency = str(reg[1]).strip()
+        elif(str(reg[3]).strip() != ""):
+          item.currency = str(reg[1]).strip()
+        else:
+          print("Cannot find product currency")
+      else:
+        raise Exception("PROCESS", "Failed to get price from string {0}".format(temp))
+
+    # ITEM OUT OF STOCK or AMAZON PLACE "Available from these sellers." span script
+    if (not result):
+      # /html/body/div[2]/div[2]/div[8]/div[4]/div[4]/div[19]/div[1]/span
+      # //*[@id="availability"]/span
+      result, ele = get_element(driver, '//*[@id="availability"]/span')
+      if (not result):
+        print("Failed to find amazon html element {0}".format("product-availability"))
+      else:
+        # OUT OF STOCK
+        if (ele.text.strip() == ""):
+          item.status_str = ele.text.strip()
+          item.status = ItemStatus.OUT_STOCK
+          item.price = 0
+          item.currency = "$"
+          print("Item {0} is out of stock".format(item_idx))
+        # Available from these sellers.
+        else:
+          # get price from vender offer option
+          result = False
+    # ITEM HAS MULTIPLE VENDOR
+    # /html/body/div[2]/div[2]/div[9]/div[4]/div[4]/div[44]
+    # /html/body/div[2]/div[2]/div[8]/div[4]/div[4]/div[44]/div[2]/span/a
+    # //*[@id="olp_feature_div"]
+    if (not result):
+      result, ele = get_element(driver, '//*[@id="olp_feature_div"]/div/span/a[@class="a-link-normal"]/span[@class="a-size-base a-color-price"]')
+      if (not result):
+        raise Exception("WEB_DRIVER", "Failed to find product price, please update the target element xpath")
+      else:
+        item.multi_vendor = True
+        item.status = ItemStatus.IN_STOCK
+        temp = ele.text.strip()
+        reg = re.match(r"([^\d\.]*)([\d\.]+)([^\d\.]*)",temp)
+        if (reg != None):
+          item.price = float(str(reg[2]))
+          if(str(reg[1]).strip() != ""):
+            item.currency = str(reg[1]).strip()
+          elif(str(reg[3]).strip() != ""):
+            item.currency = str(reg[1]).strip()
+          else:
+            print("Cannot find product currency")
+        else:
+          raise Exception("PROCESS", "Failed to get price from string {0}".format(temp))
+    if (result):
+      print("COLLECT ITEM {0} DONE - {1}{2}".format(item_idx, item.price, item.currency))
   driver.quit()
 
-def update_excel(prod_list):
-
-  pass
+def update_excel(ws, prod_list):
+  for item in prod_list[1]:
+    ws.cell(item.row_idx, COL_AMA_PRICE).value = item.price
+    ws.cell(item.row_idx, COL_ITEM_TITLE).value = item.name
+    ws.cell(item.row_idx, COL_AMA_STS).value = ITEM_STATUS[item.status]
+  print("--- DONE ----")
 
 def main(args)->None:
   wb = openpyxl.load_workbook(args[1])
@@ -108,15 +221,16 @@ def main(args)->None:
     for idx, col in enumerate([COL_EBAY_LINK, COL_AMA_LINK]):
       val = ws.cell(row, col).value
       if ((val != None) and (val != "")):
-        temp_prod = Product(_site=col,_link=val)
+        temp_prod = Product(_site=col,_link=val,_row_idx=row)
         prod_list[idx].append(temp_prod)
 
   get_price_ebay(prod_list[0])
 
   get_price_amazon(prod_list[1])
 
-  update_excel(prod_list)
-
+  update_excel(ws, prod_list)
+  wb.save(args[1])
+  wb.close()
   pass
 
 if __name__ == "__main__":
