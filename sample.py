b50effd9adad3d17edc68d8aac4cb82d8492383c
DungNv <dungnv66@vingroup.net>
2021-03-03 16:22:39 +0700

    update selenium

--- sample.py
+++ sample.py
@@ -9,7 +9,11 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
 COL_EBAY_LINK = 2
 COL_ITEM_TITLE = 4
 COL_AMA_LINK = 5
@@ -44,8 +48,22 @@ def get_price_ebay(prod_list):
 
   pass
 
+def click_element(driver, xpath):
+  try:
+    wait(driver, 5).until(EC.element_to_be_clickable((By.XPATH,xpath))).click()
+    return True
+  except:
+    return False
+def send_data(driver, xpath, data):
+  try:
+    wait(driver, 5).until(EC.presence_of_element_located((By.XPATH,xpath))).send_keys(data)
+    return True
+  except:
+    return False
+
 def get_price_amazon(prod_list):
   # TODO: can get data faster with request(grequest), but has to change the zipcode to correct location with the request?
+  #       issue: ip block with multiple request
   # header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"}
   # rs = [requests.get(item.link, headers=header) for item in prod_list]
   # for idx,response in enumerate(rs):
@@ -60,7 +78,21 @@ def get_price_amazon(prod_list):
   options.add_argument("--incognito")
   # options.add_argument("--headless")
   driver = webdriver.Chrome("./browserdriver/ChromeDriver88.0.4324.96_32b.exe", chrome_options=options)
-  driver.get(prod_list[0].link)
+  driver.get("https://amazon.com")
+
+  # find zipcode element
+  click_element(driver, '//*[@id="nav-global-location-popover-link"]')
+  # //*[@id="GLUXZipUpdateInput"]
+  send_data(driver, '//*[@id="GLUXZipUpdateInput"]', "97124")
+  # GLUXZipUpdate - update code button
+  click_element(driver, '//*[@id="GLUXZipUpdate"]')
+  # continue - GLUXConfirmAction - /html/body/div[5]/div/div/div[2]/span/span
+  if (False == click_element(driver,'//*/div[@class="a-popover-wrapper"]/div[@class="a-popover-footer"]/span/span')):
+    # a-autoid-29-announce - apply code
+    click_element(driver,'//*[@id="a-autoid-29-announce"]')
+  for item in prod_list:
+    driver.get(item.link)
+
   driver.quit()
 
 def update_excel(prod_list):
